53b6ae31b33eba965906d09fa5a5cb6adff59417
leandrookamada <leandrolobatokmd@gmail.com>
2025-07-07 21:11:28 -0300

    criando a calculadora

--- PYTHON/exercícios de logica/ex03.py
+++ PYTHON/exercícios de logica/ex03.py
@@ -1,8 +1,20 @@
 #FUNÇÕES 
 """
 No python, as funções são chamadas por 'def' nome_da_função(parametros):
+    elas fazem parte da função enquanto estão dentro do escopo dela, que diferente de outras linguagens, é definida pela distancia do inicio da linha.
 """
 def calcula_valores(x, y):
     print(x + y)
     
 calcula_valores(5, 8)    
+
+"""ARGUMENTOS!!"""
+"""Os Argumentos podem ser passados dentro do parámentro, e pode se usar quantos quiser, apenas os separando por vírgula"""
+
+def fala_ola_mundo(tiposDeOla):
+    print(tiposDeOla, "word")
+fala_ola_mundo("Olá")
+fala_ola_mundo("hello")
+
+b = "Hello, World!"
+print(b[2:5])
